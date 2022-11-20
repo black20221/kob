4e63fc9b16fda0d746d336335f74a96b098ef491
lee <1776130741@qq.com>
2022-11-20 23:27:22 +0800

    导航栏和对战地图

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,6 +1,44 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import PkIndexview from '../views/pk/PkIndexview'
+import RecordIndexview from '../views/record/RecordIndexview'
+import RanklistIndexview from '../views/ranklist/RanklistIndexview'
+import UserbotsIndexview from '../views/user/bots/UserbotsIndexview'
+import NotFound from '../views/error/NotFound'
 const routes = [
+  {
+    path: "/",
+    name: "home",
+    redirect: "/pk/",
+  },
+  {
+    path: "/pk/",
+    name: "pk_index",
+    component: PkIndexview,
+  },
+  {
+    path: "/record/",
+    name: "record_index",
+    component: RecordIndexview,
+  },
+  {
+    path: "/ranklist/",
+    name: "ranklist_index",
+    component: RanklistIndexview,
+  },
+  {
+    path: "/user/bots/",
+    name: "user_bot_index",
+    component: UserbotsIndexview,
+  },
+  {
+    path: "/404/",
+    name: "404",
+    component: NotFound,
+  },
+  {
+    path: "/:catchAll(.*)",
+    redirect: "/404/",
+  },
 ]
 
 const router = createRouter({
---
+++ web/src/views/error/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+    <ContentField>
+        404NO Found
+    </ContentField>
+</template>
+<script>
+import ContentField from '../../components/ContentField.vue'
+export default {
+    components: {
+        ContentField
+    }
+}
+</script>
+<style scoped>
+
+</style>
